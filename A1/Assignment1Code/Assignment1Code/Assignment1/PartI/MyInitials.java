d319d608802ed58b38f26528858a9047fc4c3239
Anil Poonai <50726395+DevonARP@users.noreply.github.com>
2023-06-13 19:50:15 -0400

    Add files via upload

---
+++ A1/Assignment1Code/Assignment1Code/Assignment1/PartI/MyInitials.java
@@ -0,0 +1,14 @@
+
+public class MyInitials {
+
+	public static void main(String[] args) {
+		System.out.println("  A   RRRR  PPPP ");
+		System.out.println(" A A  R   R P   P");
+		System.out.println("A   A R   R P   P");
+		System.out.println("AAAAA RRRR  PPPP ");
+		System.out.println("A   A R R   P    ");
+		System.out.println("A   A R  R  P    ");
+		System.out.println("A   A R   R P    ");
+
+	}
+}
